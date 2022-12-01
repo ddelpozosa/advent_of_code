96bf59c5053291338d12431c9d1c639af754317b
Daniel Pozo <dpozo@team.mobile.de>
2022-12-01 10:54:27 +0100

    day 1

--- 2022/1/day1.py
+++ 2022/1/day1.py
@@ -1,9 +1,31 @@
 import os
 
 from pathlib import Path
-p = Path(__file__).with_name('test.txt')
+p = Path(__file__).with_name('input.txt')
 
 f = p.open('r')
 lines = f.readlines()
+lines.append("\n")
 
-print(lines)
+topThree = [0,0,0]
+highestElf = 0
+currentElf = 0
+for line in lines:
+    if line == "\n":
+        ### Part 1 ###
+        if highestElf < currentElf:
+            highestElf = currentElf
+        
+        ### Part 2 ###
+        topThree.append(currentElf)
+        topThree.remove(min(topThree))
+
+        currentElf = 0
+    else:
+        currentElf += int(line)
+
+print(f'The elf with the most calories has {highestElf} calories')
+
+print(f'The top 3 elfs have the following calories: {topThree}')
+
+print(f'The top 3 elfs have a total of {sum(topThree)} calories')
